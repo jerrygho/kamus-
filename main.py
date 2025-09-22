c661282c7fda3d71aa00f2a66e28ccf0636ed370
jerrygho <jeremyadrielgho17082011@gmail.com>
2025-09-22 20:22:05 +0700

    Create main.py
    gg

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "OTW":"menuju suatu tempat",
+            "GTG":"ingin pergi"
+            }
+
+while True:
+    word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print('Kata tersebut tidak ada dalam kamus')
+    
